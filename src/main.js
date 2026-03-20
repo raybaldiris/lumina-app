0f47b3142744ad6eb45a3d785e3d081811dee4b9
Ray Baldiris <raybrandesign@gmail.com>
2026-03-20 10:38:11 -0500

    init: estructura base lumina con vite

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import './css/reset.css'
+import './css/tokens.css'
+import './css/semantic.css'
+import './css/main.css'
+
+document.querySelector('#app').innerHTML = `
+<h1>Lumina - App </h1>
+`
+
+setupCounter(document.querySelector('#counter'))
